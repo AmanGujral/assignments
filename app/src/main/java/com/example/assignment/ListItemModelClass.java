f9db5004508b78c04a638f4595e26b76fdca8cb0
Amanpreet Singh <36414846+AmanGujral@users.noreply.github.com>
2020-11-19 11:20:08 -0500

    Added ListItemModelClass in java.

---
+++ app/src/main/java/com/example/assignment/ListItemModelClass.java
@@ -0,0 +1,57 @@
+package com.example.assignment;
+
+public class ListItemModelClass {
+
+    private String title;
+    private String rating;
+    private String description;
+    private String imgSrc;
+
+    //default constructor
+    ListItemModelClass(String title, String rating, String description, String imgSrc){
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.imgSrc = imgSrc;
+    }
+
+    //constructor without imgSrc parameter
+    ListItemModelClass(String title, String rating, String description){
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.imgSrc = String.valueOf(R.drawable.default_image);//default image when no image provided
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+}
